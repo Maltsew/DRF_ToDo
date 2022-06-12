9a681ee8b9d1dde24934ea0122f1511f8f62131e
Malcev Nikita <maltsew.na@gmail.com>
2022-06-12 20:11:11 +0300

    Просто REST API на DRF

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    # поля модели, которые видны в списке
+    list_display = ('id', 'title', 'description', 'date', 'done')
+    list_display_links = ('id', 'title')
+    # по каким полям модели возможен поиск
+    search_fields = ('id', 'title', 'description')
+    # модификация в списке атрибутов
+    list_editable = ('done',)
+    list_filter = ('done',)
+
+
+admin.site.register(Todo, TodoAdmin)
+
